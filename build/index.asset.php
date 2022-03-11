09041b19ae815197f8da26a03a300b5419ddc68c
jeherve <jeherve@users.noreply.github.com>
2022-03-11 10:46:28 +0000

    Introduce Jetpack Reach plugin (#23232)
    Co-authored-by: Jeremy Herve <jeremy@jeremy.hu>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/1968293736

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '13631452c5905bb448af6d60a0b4c855');
